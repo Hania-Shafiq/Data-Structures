87aa990f806ce8555347cbaf6c309ac9b3c5f802
Hania Shafiq <140416218+Hania-Shafiq@users.noreply.github.com>
2024-11-08 14:47:23 +0500

    Update InsertionSort.cpp

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,33 +1,39 @@
 //INSERTION SORT
-#include <iostream>
+#include<iostream>
 using namespace std;
+
+//INSERTION SORT
 void insertionSort(int arr[],int n){
-    for(int i=1;i<n;i++){ //unsorted part
+    for(int i=1;i<n;i++){ //unsorted array
         int current=arr[i];
-        int j=i-1; //sorted part
-        while(arr[j]>current && j>=0){
+        int j=i-1;
+        while(j>=0 && arr[j]>current){//sorted array
             arr[j+1]=arr[j];
             j--;
         }
         arr[j+1]=current;
     }
 }
+
 int main(){
     int n;
-    cout<<"Enter the number of elements:\n";
+    cout<<"INPUT ARRAY SIZE:\n";
     cin>>n;
-    int array[n];
-    cout<<"Enter the elements of the array:\n";
+    cout<<"INPUT ARRAY ELEMENTS;\n";
+    int arr[n];
+    //input array
     for(int i=0;i<n;i++){
-        cin>>array[i];
+        cin>>arr[i];
     }
-    insertionSort(array,n);
-            cout<<"Sorted array:\n";
+    insertionSort(arr,n);
+    //output array
+    cout<<"SORTED ARRAY:\n";
     for(int i=0;i<n;i++){
-        cout<<array[i]<<" ";
+        cout<<arr[i]<<" ";
     }
     return 0;
 }
+
 
 //Time complexity: O(n^2)
 //Auxiliary Space Complexity: O(1)
