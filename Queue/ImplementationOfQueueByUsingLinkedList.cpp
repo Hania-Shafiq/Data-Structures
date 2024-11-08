ce9b797171a6cbd171645b04acf620184b8731a1
Hania Shafiq <140416218+Hania-Shafiq@users.noreply.github.com>
2024-11-08 11:44:11 +0500

    Update ImplementationOfQueueByUsingLinkedList.cpp

--- Queue/ImplementationOfQueueByUsingLinkedList.cpp
+++ Queue/ImplementationOfQueueByUsingLinkedList.cpp
@@ -1,4 +1,3 @@
-//IMPLEMENTATION OF QUEUE USING LINKEDLIST
 #include <iostream>
 using namespace std;
 
@@ -10,18 +9,19 @@ struct Node{
 class Queue{
     Node* front;
     Node* back;
-    public:
+public:
     Queue(){
-        front==back==NULL;
+        front=back=NULL;
     }
     
     //ENQUE
     void enque(int val){
         Node* newNode=new Node();
         newNode->data=val;
-        newNode->next=NULL:
-        if(back==NULL){
-            front==back==newNode;
+        newNode->next=NULL;
+        cout<<val<<" enqued\n";
+        if(front==NULL){
+            front=back=newNode;
             return;
         }
         back->next=newNode;
@@ -30,9 +30,69 @@ class Queue{
     
     //DEQUE
     void deque(){
-        Node* newNode=new Node();
         if(front==NULL){
-            
-        } //will continue it tommorow
+            cout<<"Queue is empty! Nothing to deque\n";
+            return;
+        }
+        Node* temp=front;
+        cout<<front->data<<" dequed\n";
+        front=front->next;
+        if(front==NULL){
+            back=NULL;
+        }
+        delete temp;
+    }
+    
+    //GETFRONT
+    int getFront(){
+        if(front==NULL){
+            cout<<"Queue is empty\n";
+            return -1;
+        }
+        return front->data;
+    }
+    
+    //GETBACK
+    int getBack(){
+        if(back==NULL){
+            cout<<"Queue is empty\n";
+            return -1;
+        }
+        return back->data;
+    }
+    
+    //IsEMPTY
+    bool isEmpty(){
+        return front==NULL;
+    }
+    
+    //DISPLAY
+    void display(){
+        if(front==NULL){
+            cout<<"Queue is Empty\n";
+            return;
+        }
+        Node* newNode=new Node();
+        Node* temp=front;
+        while(temp!=NULL){
+            cout<<temp->data<<"->";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
     }
 };
+int main(){
+    Queue q;
+    q.enque(1);
+    q.enque(2);
+    q.enque(3);
+    q.enque(6);
+    q.enque(8);
+    q.enque(9);
+    q.deque();
+    q.deque();
+    cout<<"front "<<q.getFront()<<endl;
+    cout<<"back "<<q.getBack()<<endl;
+    q.display();
+    return 0;
+}
